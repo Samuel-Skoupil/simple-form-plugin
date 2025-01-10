d96b310b4762065d9b91cf672299849e77dd1f3e
Samuel-Skoupil <136725927+Samuel-Skoupil@users.noreply.github.com>
2025-01-10 14:07:11 +0100

    Adding functionality to move thru steps.

--- simple-form-plugin.php
+++ simple-form-plugin.php
@@ -5,13 +5,25 @@ Author: Samuel Skoupil
 */
 
 function simple_line () {
-    return '<form id="form-step-1" class="form" method="post">
+    return '<form id="simple-form" method="post">
+              <div id="form-step-1" class="form">
               <label for="name">Name</label>
               <input type="text" id="name" name="name" required placeholder="Enter your name">
                 <div class="button-container">
                 <button type="button" onClick="show_next_page()">Next step</button>
                 </div>
+              </div>  
+
+              <div id="form-step-2" class="form">
+              <label for="last_name">Last Name</label>
+              <input type="text" id="last_name" name="last_name" required placeholder="Enter your last name">
+                <div class="button-container">
+                <button type="submit" onClick="show_next_page()">Submit</button>
+                </div>
+              </div>  
             </form>';
+
+            
 }
 
 add_shortcode("simple_form", "simple_line");
