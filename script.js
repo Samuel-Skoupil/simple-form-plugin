0b043e8b9eeb369ea8100586fa5fc65b18b01874
Samuel-Skoupil <136725927+Samuel-Skoupil@users.noreply.github.com>
2025-01-10 13:23:32 +0100

    Implementing js

---
+++ script.js
@@ -0,0 +1,4 @@
+function show_next_page() {
+  document.getElementById("form-step-1").style.display = "none";
+  document.getElementById("form-step-2").style.display = "block";
+}
--- simple-form-plugin.php
+++ simple-form-plugin.php
@@ -5,19 +5,21 @@ Author: Samuel Skoupil
 */
 
 function simple_line () {
-    return '<form class="form" method="post">
+    return '<form id="form-step-1" class="form" method="post">
               <label for="name">Name</label>
-              <input type="text" id="name" name="name" required>
+              <input type="text" id="name" name="name" required placeholder="Enter your name">
                 <div class="button-container">
-                <button type="submit">Submit</button>
+                <button type="button" onClick="show_next_page()">Next step</button>
                 </div>
             </form>';
 }
 
 add_shortcode("simple_form", "simple_line");
 
-function add_styles () {
+function add_assets () {
     wp_enqueue_style("my-styles", plugin_dir_url(__FILE__) . "styles.css");
+    wp_enqueue_script("my-script", plugin_dir_url(__FILE__) . "script.js", array(), false, true);
+
 }
 
-add_action("wp_enqueue_scripts", "add_styles");
+add_action("wp_enqueue_scripts", "add_assets");
