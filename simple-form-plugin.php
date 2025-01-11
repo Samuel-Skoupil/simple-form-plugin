10003fb915c937e5dfcb6115558585d569c5d75c
Samuel-Skoupil <136725927+Samuel-Skoupil@users.noreply.github.com>
2025-01-11 16:30:00 +0100

    Creating forms layout.

--- simple-form-plugin.php
+++ simple-form-plugin.php
@@ -10,14 +10,35 @@ function simple_line() {
 
     // Ak je nastavenie "one", zobrazí jeden formulár
     if ($form_type === 'one') {
-        return '<form id="simple-form" method="post">
-                  <label for="name">Name <span class="required">*</span></label>
-                  <input type="text" id="name" name="name" required placeholder="Enter your name">
-                  <label for="last_name">Last Name <span class="required">*</span></label>
-                  <input type="text" id="last_name" name="last_name" required placeholder="Enter your last name">
+        return '<form id="simple-form" method="post" >
+
+                  <div class="form">
+                    <div class="form-element">
+                        <label for="name">Name <span class="required">*</span></label>
+                        <input type="text" id="name" name="name" required placeholder="Enter your name">
+                    </div>
+
+                    <div class="form-element">
+                        <label for="last_name">Last Name <span class="required">*</span></label>
+                        <input type="text" id="last_name" name="last_name" required placeholder="Enter your last name">
+                    </div>
+
+                    <div class="form-element">
+                        <label for="email">Email <span class="required">*</span></label>
+                        <input type="text" id="email" name="email" required placeholder="Enter your email adress">
+                    </div>
+
+                    <div class="form-element">
+                        <label for="phone_number">Phone Number </label>
+                        <input type="text" id="phone_number" name="phone_number" required placeholder="Enter your phone number">
+                    </div>
+
+                  </div>
+
                   <div class="button-container">
                       <button type="submit">Submit</button>
                   </div>
+
                 </form>';
     }
 
