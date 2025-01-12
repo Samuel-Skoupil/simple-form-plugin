c030d2f0cdd649a26917ab77cc9be42eeebe23f2
Samuel-Skoupil <136725927+Samuel-Skoupil@users.noreply.github.com>
2025-01-12 17:38:47 +0100

    One step Form (TO DO: adjust style to select and input type=date,)

--- ajax-handler.php
+++ ajax-handler.php
@@ -7,15 +7,27 @@ function handle_simple_form_submission()
     $last_name = sanitize_text_field($_POST["last_name"]);
     $email = sanitize_email($_POST["email"]);
     $phone_number = sanitize_text_field($_POST["phone_number"]);
+    $state = sanitize_text_field($_POST["state"]);
+    $city = sanitize_text_field($_POST["city"]);
+    $date_of_birth = sanitize_text_field($_POST["date_of_birth"]);
+    $gender = sanitize_text_field($_POST["gender"]);
 
+    // Get recipient email from settings
     $to_email = get_option("simple_form_email");
 
+    // Create email subject and message
     $subject = "New form submission";
-    $message = "Name: $name\nLast Name: $last_name\nEmail: $email\nPhone Number: $phone_number";
-
+    $message = "Name: $name\n";
+    $message .= "Last Name: $last_name\n";
+    $message .= "Email: $email\n";
+    $message .= "Phone Number: $phone_number\n";
+    $message .= "State: $state\n";
+    $message .= "City: $city\n";
+    $message .= "Date of Birth: $date_of_birth\n";
+    $message .= "Gender: $gender\n";
     $sent = wp_mail($to_email, $subject, $message);
-    error_log('Sending email to: ' . $to_email);
 
+    error_log('Sending email to: ' . $to_email);
 
     if ($sent) {
         wp_send_json_success("Form submitted successfully");
--- templates/form-one.php
+++ templates/form-one.php
@@ -1,6 +1,6 @@
 <form id="simple-form-one" method="post">
-
     <div class="form">
+        <!-- Existing fields -->
         <div class="form-element">
             <label for="name">Name <span class="required">*</span></label>
             <input type="text" id="name" name="name" required placeholder="Enter your name">
@@ -13,7 +13,7 @@
 
         <div class="form-element">
             <label for="email">Email <span class="required">*</span></label>
-            <input type="text" id="email" name="email" required placeholder="Enter your email adress">
+            <input type="text" id="email" name="email" required placeholder="Enter your email address">
         </div>
 
         <div class="form-element">
@@ -21,10 +21,33 @@
             <input type="text" id="phone_number" name="phone_number" required placeholder="Enter your phone number">
         </div>
 
+        <!-- New fields -->
+        <div class="form-element">
+            <label for="state">State</label>
+            <input type="text" id="state" name="state" placeholder="Enter your state">
+        </div>
+
+        <div class="form-element">
+            <label for="city">City</label>
+            <input type="text" id="city" name="city" placeholder="Enter your city">
+        </div>
+
+        <div class="form-element">
+            <label for="date_of_birth">Date of Birth</label>
+            <input type="date" id="date_of_birth" name="date_of_birth" placeholder="Enter your date of birth">
+        </div>
+
+        <div class="form-element">
+            <label for="gender">Gender</label>
+            <select id="gender" name="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+            </select>
+        </div>
     </div>
 
     <div class="button-container">
         <button type="submit">Submit</button>
     </div>
-
 </form>
