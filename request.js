00bb22f11801ec1a6935eca4e4b6832834b5e91a
Samuel-Skoupil <136725927+Samuel-Skoupil@users.noreply.github.com>
2025-01-12 16:41:17 +0100

    Refactoring js for submiting form.

--- request.js
+++ request.js
@@ -1,7 +1,9 @@
-// One-Step Form
-document
-  .getElementById("simple-form-one")
-  .addEventListener("submit", function (e) {
+function handleFormSubmission(formId) {
+  const form = document.getElementById(formId);
+
+  if (!form) return; // Ak formulár neexistuje, preskočíme
+
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     const formData = new FormData(this);
@@ -31,37 +33,8 @@ document
           "<p>An error has occurred. Please try again later.</p>";
       });
   });
+}
 
-// Two-Step Form
-document
-  .getElementById("simple-form-two")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const formData = new FormData(this);
-    formData.append("action", "handle_simple_form_submission");
-    formData.append("security", simple_form_ajax.nonce);
-
-    fetch(simple_form_ajax.ajax_url, {
-      method: "POST",
-      body: formData,
-      headers: {
-        "X-WP-Nonce": simple_form_ajax.nonce,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          document.querySelector(".button-container").innerHTML =
-            "<p>Form has been submitted successfully</p>";
-        } else {
-          document.querySelector(".button-container").innerHTML =
-            "<p>Something went wrong</p>";
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        document.querySelector(".button-container").innerHTML =
-          "<p>An error has occurred. Please try again later.</p>";
-      });
-  });
+// Pripojíme funkciu k obom formulárom
+handleFormSubmission("simple-form-one");
+handleFormSubmission("simple-form-two");
